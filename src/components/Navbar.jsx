6b35a1093b2d5999d88f143c372b2c39493f0843
shivam79315 <158444124+shivam79315@users.noreply.github.com>
2025-02-02 22:58:08 +0530

    🛠️ ️ Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "../styles/Navbar.module.css";
+import { useValue } from "../itemContext";
+
+function Navbar() {
+  const { item, total, clear } = useValue();
+  return (
+    <div className={styles.container}>
+      <h1>Total : &#x20B9; {total}</h1>
+      <h1>Items: {item}</h1>
+      <div className={styles.buttonsWrapper}>
+        <button className={styles.button} onClick={clear}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
